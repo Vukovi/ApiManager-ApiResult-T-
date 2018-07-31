bba74dc35e60da768369b429fbbe249dcdc55dbe
vknezevic <vuk.knezevic@nfinnova.com>
2018-07-31 12:20:12 +0200

    Weather RU app

---
+++ WeatherApp/ErrorManager.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorManager.swift
+//  WeatherApp
+//
+//  Created by Ivan Akulov on 30/08/16.
+//  Copyright © 2016 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+public let SWINetworkingErrorDomain = "ru.swiftbook.WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
+public let UnexpectedResponseError = 200
